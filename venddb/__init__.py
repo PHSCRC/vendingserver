2f3e8920a58f11d241b5fe862aba3eeedd57c9e3
Aaron Olkin <aaronolkin@gmail.com>
2016-05-11 11:24:51 -0400

    Added venddb and config apps (the latter a submodule), adjusted gitignore and settings

---
+++ venddb/__init__.py
@@ -0,0 +1,2 @@
+
+default_app_config = "venddb.apps.VendDBConfig"
---
+++ venddb/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class VendDBConfig(AppConfig):
+    name = 'venddb'
+    verbose_name = "Vending Database"
